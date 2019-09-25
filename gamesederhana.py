cdb7ee5dd10448270fe6174496494e854e239338
aldae7 <aldinofajrian2@mhs.pelitabangsa.ac.id>
2019-09-26 03:11:57 +0700

    gamesederhana

---
+++ gamesederhana.py
@@ -0,0 +1,82 @@
+import turtle
+import os
+
+# setup screen
+
+wn = turtle.Turtle()
+wn = turtle.Screen()
+wn.bgcolor("black")
+wn.title("Pesawat luar angkasa")
+
+
+#Garis2nya
+
+border_pen = turtle.Turtle()
+border_pen.speed(0)
+border_pen.color("white")
+border_pen.penup()
+border_pen.setposition(-300,-300)
+border_pen.pendown()
+border_pen.pensize(3)
+for side in range(4):
+    border_pen.fd(600)
+    border_pen.lt(90)
+border_pen.hideturtle()
+
+#membuat pemain nya
+
+player=turtle.Turtle()
+player.color("blue")
+player.shape("triangle")
+player.penup()
+player.speed(0)
+player.setposition(0, -250)
+player.setheading(90)
+
+playerspeed = 15
+
+# membuat musuhnya...
+enemy=turtle.Turtle()
+enemy.color("red")
+enemy.shape("circle")
+enemy.penup()
+enemy.speed(0)
+enemy.setposition(-200, 250)
+
+enemyspeed = 2
+
+#perpindahan pemainnya
+
+def move_left():
+    x = player.xcor()
+    x -= playerspeed
+    if x < -280:
+        x = - 280
+    player.setx(x)
+    
+def move_right():
+    x = player.xcor()
+    x += playerspeed
+    if x < 280:
+        x = 280
+    player.setx(x)
+
+#membuat keyboard pencetannya
+turtle.listen()
+turtle.onkey(move_left, "Left")
+turtle.onkey(move_right, "Right")
+
+#main game loop
+while True:
+    #perpindahan musuh
+    x = enemy.xcor()
+    x += enemyspeed
+    enemy.setx(x)
+
+    #perpindahan musuh back dn bawah
+    if enemy.xcor() > 280:
+        enemyspeed *= -1
+    if enemy.xcor() < -280:
+        enemyspeed *= -1
+
+delay = raw_input("masukkan untuk selesai")
